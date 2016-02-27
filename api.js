332cd4eb88565ab0df08f81aef4330756bcd3b8c
Duke Gallo <dukegallo@gmail.com>
2016-02-27 15:01:00 -0800

    API

--- api.js
+++ api.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var request = require('request');
 var bodyParser = require('body-parser');
 var textParser = bodyParser.text();
-
 router.post('/', textParser, function(req,res) {
   var input = req.body;
   var url = "http://api.songkick.com/api/3.0/search/artists.json?query="+ input + "&apikey=w3AokocXm77pNg3k" ;
@@ -15,7 +14,6 @@ router.post('/', textParser, function(req,res) {
       'Content-Type': 'application/json'
     }
   };
-
 request.get(options, function (error, response, body) {
   if (!error && res.statusCode == 200) {
     res.send(body);
@@ -23,12 +21,10 @@ request.get(options, function (error, response, body) {
     }
   });
 });
-
 request.get(options, function (error, response, body) {
   if (!error && res.statusCode == 200) {
     res.send(body);
     }
   });
 });
-
 module.exports = router;
