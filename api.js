ed21f262b48fa124ad8e2b1987079a0c884d5b1f
Duke Gallo <dukegallo@gmail.com>
2016-03-08 11:02:08 -0800

    api updated.

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var bodyParser = require('body-parser');
-var textParser = bodyParser.text();
+var textParser = bodyParser.text();ls
 router.post('/', textParser, function(req,res) {
   var input = req.body;
   var url = "http://api.songkick.com/api/3.0/search/artists.json?query="+ input + "&apikey=w3AokocXm77pNg3k" ;
